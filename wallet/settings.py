a8988d7ff58ed3912522f91c131c30c757031bf0
Srikanth Chelluri <schellluri.developer@gmail.com>
2017-12-24 16:27:30 -0500

    static files

--- wallet/settings.py
+++ wallet/settings.py
@@ -52,6 +52,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'wallet.urls'
